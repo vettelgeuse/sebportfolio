2e97cc330c6e78b245325cb168bcb4268619f10e
Sebastian <mastropieros@gmail.com>
2019-08-20 15:47:16 +0100

    implement route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { HashRouter, Route, Link } from "react-router-dom";
-// import logo from './logo.svg';
-import logo from './images/like.svg';
+import Home from './home';
+import Inside from './inside';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h2>This is Sebastian's Portfolio</h2>
-        </p>
-        <a
-          className="App-link"
-          href="/inside"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1>Everything you wanted to know about me!</h1>
-        </a>(but were afraid to ask)
-      </header>
+      <HashRouter>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/details' component={Inside} />
+        {/* <Home/>
+        <Inside/> */}
+      </HashRouter>
     </div>
   );
 }
