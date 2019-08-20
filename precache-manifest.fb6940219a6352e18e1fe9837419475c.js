5d03cd308e5781da012ac8258fcc9f45a8966bcf
Sebastian <mastropieros@gmail.com>
2019-08-20 11:28:41 +0100

    Updates

---
+++ precache-manifest.fb6940219a6352e18e1fe9837419475c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a6105f986fa3d12e31f78064c63f3a",
+    "url": "/sebportfolio/index.html"
+  },
+  {
+    "revision": "aba078448790a9a626a9",
+    "url": "/sebportfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2ca10786b8eff17eeda2",
+    "url": "/sebportfolio/static/js/2.3b809774.chunk.js"
+  },
+  {
+    "revision": "aba078448790a9a626a9",
+    "url": "/sebportfolio/static/js/main.6fdd6e60.chunk.js"
+  },
+  {
+    "revision": "aea91a16dd287c61dbc0",
+    "url": "/sebportfolio/static/js/runtime~main.dddc418b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sebportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
